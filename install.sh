e1823d42e264a799ca10ef3375420b388c080042
Jan Stransky <jan.stransky@arnal.cz>
2013-09-24 08:56:32 +0200

    update install

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
+# install homeshick
 git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 chmod +x $HOME/.homesick/repos/homeshick/bin/homeshick.csh
+printf '\nalias homeshick source "$HOME/.homesick/repos/homeshick/bin/homeshick.csh"' >> $HOME/.cshrc
+
+# install my castle
 $HOME/.homesick/repos/homeshick/bin/homeshick clone https://github.com/burningtree/dotfiles
