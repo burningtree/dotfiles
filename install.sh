0a1e5e545b35067d6d73697429a9c0d3783f22dd
Jan Stransky <jan.stransky@arnal.cz>
2013-09-24 08:21:18 +0200

    fix install

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
-chmod +x $HOME/.homesick/repos/homeshick
+chmod +x $HOME/.homesick/repos/homeshick/bin/homeshick.csh
 $HOME/.homesick/repos/homeshick/bin/homeshick clone https://github.com/burningtree/dotfiles
