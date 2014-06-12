dc6415c11a5d5dbce4ff425845180d45211007c3
Jan Stransky <jan.stransky@arnal.cz>
2014-06-12 06:54:36 +0200

    Update install.sh.

--- install.sh
+++ install.sh
@@ -2,12 +2,9 @@
 
 # install homeshick
 git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
-chmod +x $HOME/.homesick/repos/homeshick/bin/homeshick.csh
-
-# update .cshrc
-printf '\nalias homeshick source "$HOME/.homesick/repos/homeshick/bin/homeshick.csh"' >> $HOME/.cshrc
+chmod +x $HOME/.homesick/repos/homeshick/bin/homeshick
 
 # install my castle
 $HOME/.homesick/repos/homeshick/bin/homeshick clone https://github.com/burningtree/dotfiles
 
-printf "Done, to link run: 'source ~/.cshrc; homeshick link dotfiles'.\n"
+printf "Done.\n"
