2323a5e830171526214c53e4f1a8b7feac59c90b
Jan Stránský <jan.stransky@arnal.cz>
2015-04-09 02:55:10 +0200

    Update vim bundles, and few tools to Brewfile and update fish.

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,10 +1,14 @@
 # disable welcome message
-set fish_greeting ""
+set fish_greeting ">>>" (uname -rs) "@" (hostname)
 
 # remove useless shell process name from terminal window
 function fish_title; pwd; end
+
+# vi(m) mode? not yet
+#set fish_key_bindings fish_vi_key_bindings
 
 alias mtr="sudo mtr"
 source $HOME/.homesick/repos/homeshick/homeshick.fish
 
 set PATH $PATH /usr/local/bin /usr/local/sbin
+set EDITOR vim
